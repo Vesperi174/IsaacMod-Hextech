1fb817a4b8e2d860f50d0befac21466fc3aebab3
Vesperi <1023625028@qq.com>
2026-07-25 15:16:13 +0800

    feat: first step

--- scripts/constants.lua
+++ scripts/constants.lua
@@ -1,3 +1,9 @@
 local mod = HextechMod
 
--- TODO
+mod.HEXTECH_CONFIG = {
+    PEDESTAL_COUNT = 3,
+    PEDESTAL_SPACING = 80,
+    PEDESTAL_Y_OFFSET = -60,
+    MAX_STAGE = 4,
+    ITEM_POOL = ItemPoolType.POOL_DEVIL,
+}
