611c873c38fc49286907f378d8519f03066ed523
Vesperi <1023625028@qq.com>
2026-07-26 12:41:51 +0800

    修复一板一眼：缓存基值攻速，修复锁定后攻击力加成清零的问题

--- scripts/items/slowandsteady.lua
+++ scripts/items/slowandsteady.lua
@@ -17,6 +17,9 @@ function mod:onSlowAndSteadyCache(player, flag)
     if flag & CacheFlag.CACHE_DAMAGE ~= 0 then
         playerData[idx].baseDamage = player.Damage
     end
+    if flag & CacheFlag.CACHE_FIREDELAY ~= 0 then
+        playerData[idx].baseMaxFireDelay = player.MaxFireDelay
+    end
 end
 
 function mod:onSlowAndSteadyUpdate(player)
@@ -28,20 +31,27 @@ function mod:onSlowAndSteadyUpdate(player)
     end
     local data = playerData[idx]
 
-    if not data.baseDamage then
+    if not data.baseDamage or not data.baseMaxFireDelay then
         data.baseDamage = player.Damage
+        data.baseMaxFireDelay = player.MaxFireDelay
         player:AddCacheFlags(CacheFlag.CACHE_DAMAGE | CacheFlag.CACHE_FIREDELAY)
         player:EvaluateItems()
         return
     end
 
+    local lockedFireDelay = 30 / TARGET_TEARS - 1
     local currentMaxFireDelay = player.MaxFireDelay
-    local normalTears = 30 / (currentMaxFireDelay + 1)
+
+    if math.abs(currentMaxFireDelay - lockedFireDelay) > 0.5 then
+        data.baseMaxFireDelay = currentMaxFireDelay
+    end
+
+    local normalTears = 30 / (data.baseMaxFireDelay + 1)
     local delta = normalTears - TARGET_TEARS
     local damageBonus = delta * CONVERSION_RATE
 
     player.Damage = data.baseDamage + damageBonus
-    player.MaxFireDelay = math.max(0, 30 / TARGET_TEARS - 1)
+    player.MaxFireDelay = lockedFireDelay
 end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.onSlowAndSteadyCache)
